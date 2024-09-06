91a35d7538ae04fb3669eae62dedf9fffded0e69
Mohammed-Zaid-Shaikh <zaidshaikh791@gmail.com>
2024-09-06 23:53:30 +0530

    [Add] Login Authentication with firebase

--- app/src/main/java/com/example/inventorymanager/login/view/activity/LoginActivity.kt
+++ app/src/main/java/com/example/inventorymanager/login/view/activity/LoginActivity.kt
@@ -2,17 +2,19 @@ package com.example.inventorymanager.login.view.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.inventorymanager.R
 import com.example.inventorymanager.databinding.ActivityLoginBinding
 import com.example.inventorymanager.home.view.activity.MainActivity
+import com.example.inventorymanager.login.viewModel.LoginViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -24,9 +26,34 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            loginUser(
+                binding.emailEditText.text.toString(),
+                binding.passwordEditText.text.toString(),
+            )
+
         }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign-in successful, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                    // Navigate to home screen
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    // If sign-in fails, display a message to the user
+                    Toast.makeText(
+                        this,
+                        "Authentication Failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
