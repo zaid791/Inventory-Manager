20c6e4e8d3e5f60b73e0281b22fcecf20ba8accc
Mohammed-Zaid-Shaikh <zaidshaikh791@gmail.com>
2024-09-30 23:16:15 +0530

    Code Optimisation, Customer and Supplier add user fragment differentiation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,12 +54,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(platform(libs.com.google.firebase.firebase.bom2))
+    implementation(platform(libs.com.google.firebase.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.analytics)
     implementation(libs.android.gif.drawable)
     implementation (libs.circleimageview)
-
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
